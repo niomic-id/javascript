328ec2b49b1cba082cb1a4bc34050d08f08da21a
Bagus ardianto <bagusardianto137@gmail.com>
2018-08-03 04:01:48 +0700

    modify obejct.js

---
+++ object.js
@@ -0,0 +1,13 @@
+function panggilObject(){
+  var mobil = {
+    type : 'Sedan',
+    harga: '200juta',
+    warna: 'Putih',
+  }
+  console.log(mobil)
+  console.log(mobil.warna)
+  console.log(mobil.type)
+  console.log(mobil.harga)
+}
+
+panggilObject()
