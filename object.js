59afd0a6aa1d2a7142264cc11ef7d4ca8325ae19
Bagus ardianto <bagusardianto137@gmail.com>
2018-08-03 03:57:00 +0700

    menambahkan file object.js

---
+++ object.js
@@ -0,0 +1,11 @@
+function panggilObject(){
+  var mobil = {
+    type : 'Sedan',
+    harga: '200juta',
+    warna: 'Putih',
+  }
+  console.log(mobil)
+  console.log(mobil.warna)
+}
+
+panggilObject()
