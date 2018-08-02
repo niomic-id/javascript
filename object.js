c251f6722579af9ab9b88636593de2b8623c3814
Bagus ardianto <bagusardianto137@gmail.com>
2018-08-03 04:12:52 +0700

    modify obejct.js

--- object.js
+++ object.js
@@ -1,13 +1,17 @@
-function panggilObject(){
-  var mobil = {
-    type : 'Sedan',
-    harga: '200juta',
-    warna: 'Putih',
+var panggilObject = () => {
+  var motor = {
+    merk : ["Honda","Yamaha","Suzuki"],
+    cc  : ["125cc","150cc","250cc"],
+    harga : ["15jt","20jt","25jt"],
+    warna : ["hitam","putih","biru"],
   }
-  console.log(mobil)
-  console.log(mobil.warna)
-  console.log(mobil.type)
-  console.log(mobil.harga)
+  console.log(motor.merk[0],"?")
+  console.log(motor.cc[2],"?")
+  console.log(motor.harga[2],"?")
+  console.log(motor.warna[1],"?")
+  console.log("Motor Apakah itu ?")
+
+  if (motor.merk[0] && motor.cc[2] && motor.harga[2] && motor.warna[1]) console.log("Honda CBR 250CC")
 }
 
 panggilObject()
